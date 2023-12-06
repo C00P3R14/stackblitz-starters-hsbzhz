f8fafa760b8bff83e5bbaeabb97c8d02cc8e637f
C00P3R14 <ngubane.linda9@gmail.com>
2023-12-06 09:41:44 +0200

    Update ProductList

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  title:string = "Life"
-  message:string = "We are on page 110"
-
-  onClick() {
-  let par = document.createElement("p")
-  let span = document.querySelector("span")
-  par.innerText = `Whoohoo!!! I made it work 😆 ${this.message} `
-  span?.appendChild(par)
-  }
+  selectedProduct = '';
 }
